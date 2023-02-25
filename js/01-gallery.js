eba2f2e8f98542eba14c76818dc9c36e00ddbed1
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-02-25 18:44:47 +0100

    add images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
 console.log(galleryItems);
