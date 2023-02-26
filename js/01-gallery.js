713251f91f20b610452588e7ab2f9f4eed6d2c31
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-02-26 21:28:09 +0100

    adds library to the task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryContainer = document.querySelector(".gallery");
-const imagesMarkup = createImagesMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+// const galleryContainer = document.querySelector(".gallery");
+// const imagesMarkup = createImagesMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 
-console.log(galleryItems);
+// function createImagesMarkup(item) {
+//     return galleryItems
+//       .map(({ preview, original, description }) => {
+//         return `<div class="gallery__item">
+//         <a class="gallery__link" href="${original.value}">
+//           <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//             alt="${description}"
+//           />
+//         </a>
+//       </div>`;
+//       })
+//       .join("");
+//   }
+//   const onContainerClick = (e) => {
+//     e.preventDefault();
+  
+//     if (e.target.classList.contains("gallery")) return;
+//       const source = e.target.dataset.source;
+      
+//     const instance = basicLightbox.create(`
+//       <img src="${source}"width="800" height="600">`);
+  
+//     instance.show();
+//   };
+  
+//   galleryContainer.addEventListener("click", onContainerClick);
