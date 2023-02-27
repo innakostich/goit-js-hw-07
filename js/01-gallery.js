21b5303836193fba24659de606a9e10cb92aad42
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-02-27 18:07:54 +0100

    adds lightbox script js and css

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// const galleryContainer = document.querySelector(".gallery");
-// const imagesMarkup = createImagesMarkup(galleryItems);
-// galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+  
+function createImagesMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
 
-// function createImagesMarkup(item) {
-//     return galleryItems
-//       .map(({ preview, original, description }) => {
-//         return `<div class="gallery__item">
-//         <a class="gallery__link" href="${original.value}">
-//           <img
-//             class="gallery__image"
-//             src="${preview}"
-//             data-source="${original}"
-//             alt="${description}"
-//           />
-//         </a>
-//       </div>`;
-//       })
-//       .join("");
-//   }
-//   const onContainerClick = (e) => {
-//     e.preventDefault();
+  const onContainerClick = (e) => {
+    e.preventDefault();
   
-//     if (e.target.classList.contains("gallery")) return;
-//       const source = e.target.dataset.source;
+    if (e.target.classList.contains("gallery")) return;
+      const source = e.target.dataset.source;
       
-//     const instance = basicLightbox.create(`
-//       <img src="${source}"width="800" height="600">`);
+    const instance = basicLightbox.create(`
+      <img src="${source}"width="800" height="600">`);
   
-//     instance.show();
-//   };
+    instance.show();
+  };
   
-//   galleryContainer.addEventListener("click", onContainerClick);
+  galleryContainer.addEventListener("click", onContainerClick);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+  
+function createImagesMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+  });
+
+ 
